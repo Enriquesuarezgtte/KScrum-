93218182d4e407e5b9e5f42581fc8a7d69b03c43
Enrique Suarez Guette <enriquesuarezgtte@gmail.com>
2019-02-17 00:50:21 -0500

    Advance to integrate Authentication with Fingerprint
    I started to advance in the develope of an authentication using FingerPrint available to Android and iOs

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio';
+
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private faio: FingerprintAIO,public loadingController: LoadingController) { }
 
   ngOnInit() {
+    
   }
-
+  login(){
+    this.faio.show({
+      clientId: 'Fingerprint-Demo',
+      clientSecret: 'password', //Only necessary for Android
+      disableBackup:true,  //Only for Android(optional)
+      localizedFallbackTitle: 'Use Pin', //Only for iOS
+      localizedReason: 'Please authenticate' //Only for iOS
+  })
+  .then((result: any) => this.presentLoadingWithOptions())
+  .catch((error: any) => console.log(error));
+  }
+  async presentLoadingWithOptions() {
+    const loading = await this.loadingController.create({
+      spinner: null,
+      duration: 5000,
+      message: 'Please wait...',
+      translucent: true,
+      cssClass: 'custom-class custom-loading'
+    });
+    return await loading.present();
+  }
 }
