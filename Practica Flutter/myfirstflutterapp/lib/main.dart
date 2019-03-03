3fdafd610f3c77ba1647b16741e3cf69e2b93d8c
Enrique Suarez Guette <enriquesuarezgtte@gmail.com>
2019-03-03 00:19:48 -0500

    Update main.dart with a stateful widget

--- Practica Flutter/myfirstflutterapp/lib/main.dart
+++ Practica Flutter/myfirstflutterapp/lib/main.dart
@@ -1,11 +1,12 @@
+
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';  // Add this line.
+import 'package:english_words/english_words.dart';
+
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-        final wordPair = new WordPair.random(); 
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
@@ -13,9 +14,22 @@ class MyApp extends StatelessWidget {
           title: const Text('Welcome to Flutter'),
         ),
         body: new Center(
-                    child: new Text(wordPair.asPascalCase),  
+          child: new RandomWords(),
         ),
-    ),
+      ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final WordPair wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
   }
 }
