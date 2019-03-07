ddb13193e5b2a53d3850cec94839240e57acdb3f
Enrique Suarez Guette <enriquesuarezgtte@gmail.com>
2019-03-07 00:34:39 -0500

    hearts tapped

--- Practica Flutter/myfirstflutterapp/lib/main.dart
+++ Practica Flutter/myfirstflutterapp/lib/main.dart
@@ -23,6 +23,36 @@ class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
     final Set<WordPair> _saved = new Set<WordPair>();  
   final TextStyle _biggerFont = const TextStyle(fontSize: 18.0); 
+   void _pushSaved() {
+      Navigator.of(context).push(
+        new MaterialPageRoute<void>(
+      builder: (BuildContext context) {
+        final Iterable<ListTile> tiles = _saved.map(
+          (WordPair pair) {
+            return new ListTile(
+              title: new Text(
+                pair.asPascalCase,
+                style: _biggerFont,
+              ),
+            );
+          },
+        );
+        final List<Widget> divided = ListTile
+          .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+          .toList();
+          return new Scaffold(         
+          appBar: new AppBar(
+            title: const Text('Saved Suggestions'),
+          ),
+          body: new ListView(children: divided),
+        );     
+      },
+    ),
+  );
+  }
  Widget _buildSuggestions() {
     return new ListView.builder(
       padding: const EdgeInsets.all(16.0),
@@ -56,14 +86,14 @@ class RandomWordsState extends State<RandomWords> {
   }
   @override
   Widget build(BuildContext context) {
-    //final wordPair = new WordPair.random(); // Delete these... 
-    //return new Text(wordPair.asPascalCase); // ... two lines.
-
-    return new Scaffold (                   // Add from here... 
+    return new Scaffold (                   
       appBar: new AppBar(
         title: new Text('Startup Name Generator'),
+        actions: <Widget>[    
+          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+        ], 
       ),
       body: _buildSuggestions(),
-    );                                      // ... to here.
+    );                                     
   }
 }
