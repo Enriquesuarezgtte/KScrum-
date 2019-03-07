781df1630180a3f68d9c4c96a8856fe4b5d2d06b
Enrique Suarez Guette <enriquesuarezgtte@gmail.com>
2019-03-07 00:21:24 -0500

    Main was modified to include hearts behind every item

--- Practica Flutter/myfirstflutterapp/lib/main.dart
+++ Practica Flutter/myfirstflutterapp/lib/main.dart
@@ -21,6 +21,7 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
+    final Set<WordPair> _saved = new Set<WordPair>();  
   final TextStyle _biggerFont = const TextStyle(fontSize: 18.0); 
  Widget _buildSuggestions() {
     return new ListView.builder(
@@ -41,11 +42,16 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
   Widget _buildRow(WordPair pair) {
+      final bool alreadySaved = _saved.contains(pair);  
     return new ListTile(
       title: new Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: new Icon(  
+      alreadySaved ? Icons.favorite : Icons.favorite_border,
+      color: alreadySaved ? Colors.red : null,
+    ), 
     );
   }
   @override
