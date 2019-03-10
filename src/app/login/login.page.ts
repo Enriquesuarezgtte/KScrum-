de3c0a05beeaa953d7f9dfa3ca257bf551e104ff
Leonardo Ruiz <leoleo1513@gmail.com>
2019-03-09 23:20:24 -0500

    Added side menu block validation

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import{Router} from '@angular/router';
+import{MenuController} from '@ionic/angular'
 
 @Component({
   selector: 'app-login',
@@ -8,7 +9,11 @@ import{Router} from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private router : Router) { }
+  constructor(private router : Router,
+    public menuController : MenuController
+    ) {
+      this.menuController.enable(false);
+     }
 
   ngOnInit() {
   }
