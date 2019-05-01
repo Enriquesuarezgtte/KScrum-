5ce85c938b67413e8737743b092aa39bf5957d97
Enrique <Enriquesuarezgtte@gmail.com>
2019-04-30 19:32:33 -0500

    fix2

--- KScrum/src/app/services/auth/auth.service.ts
+++ KScrum/src/app/services/auth/auth.service.ts
@@ -24,9 +24,11 @@ export class AuthService {
 
   currentUser: IUser;
 
+  webApiKey : string;
 
   constructor(public auth: AngularFireAuth) {
     this.currentUser = new IUser;
+     this.webApiKey = '173697716170-75cl00gilu525kpvmt1akoj0qbckcb0b.apps.googleusercontent.com';
 
   }
 
@@ -82,5 +84,46 @@ export class AuthService {
   }
 
 
+  logInWithGoogle() : Promise<any> {
 
+     return this.googlePlus.login({'webClientId': this.webApiKey })
+      .then(res => {
+        console.log(res);
+        const googleCredential = firebase.auth.GoogleAuthProvider.credential(res.idToken);
+        this.auth.auth.signInWithCredential(googleCredential).then (resolve => {
+          console.log(resolve);
+          this.currentUser.email = resolve.email;
+          this.currentUser.UUID = resolve.uid;
+          this.currentUser.displayName = resolve.displayName;
+          this.currentUser.imageUrl = resolve.photoURL;
+
+          console.log(this.currentUser);
+        }).catch(error => {
+          console.log(error);
+        });
+          return res;
+      })
+      .catch(err => {
+        console.error(err);
+        return false;
+      });
+  }
+
+  logIngWithGitHub()  {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithRedirect(provider).then(function() {
+      return firebase.auth().getRedirectResult();
+    }).then(function(result) {
+
+      var token = result.credential;
+    
+      var user = result.user;
+      // ...
+    }).catch(function(error) {
+
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+    
+  } 
 }
